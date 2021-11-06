fb89ddc5739f2454f83f85fc9e68ffe445b57314
Nejc <nejcrogelsek0@gmail.com>
2021-11-06 18:29:24 +0100

    mongoose connection

--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -1,4 +1,7 @@
 import fp from 'fastify-plugin'
+import { User } from '../entities/user.entity'
+import * as mongoose from 'mongoose'
+import { IUser } from '../interfaces/user.interface'
 
 export interface SupportPluginOptions {
   // Specify Support plugin options here
@@ -7,14 +10,27 @@ export interface SupportPluginOptions {
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
-  })
+
+  const db = await mongoose.connect('mongodb://root:example@localhost:27017', {
+		dbName: 'fastify-crash-course'
+	}).then(conn => {
+		fastify.decorate('store', {
+			User: conn.model('User', User),
+			db: conn
+		})
+
+		return conn
+	}).catch(console.error)
+
+	if (!db) throw new Error('Cannot connect to database.')
 })
 
 // When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
   export interface FastifyInstance {
-    someSupport(): string;
+    store: {
+		User: mongoose.Model<IUser>,
+		db: typeof mongoose
+	}
   }
 }
