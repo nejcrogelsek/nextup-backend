95afa6762230ab9af6893d0094907bfa776eb26d
Nejc <nejcrogelsek0@gmail.com>
2021-11-06 18:30:52 +0100

    jwt functionality

--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -2,6 +2,7 @@ import fp from 'fastify-plugin'
 import { User } from '../entities/user.entity'
 import * as mongoose from 'mongoose'
 import { IUser } from '../interfaces/user.interface'
+import fastifyJwt from 'fastify-jwt'
 
 export interface SupportPluginOptions {
   // Specify Support plugin options here
@@ -10,6 +11,17 @@ export interface SupportPluginOptions {
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
+	fastify.register(fastifyJwt, {
+		secret: 'supersecret',
+		sign: {
+			expiresIn: '15 minutes'
+		}
+	})
+
+	fastify.decorate('generateJwt', (email: string) => {
+		return fastify.jwt.sign({ email })
+	})
+
 
   const db = await mongoose.connect('mongodb://root:example@localhost:27017', {
 		dbName: 'fastify-crash-course'
@@ -28,6 +40,7 @@ export default fp<SupportPluginOptions>(async (fastify, opts) => {
 // When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
   export interface FastifyInstance {
+	generateJwt: (email: string) => string
     store: {
 		User: mongoose.Model<IUser>,
 		db: typeof mongoose
