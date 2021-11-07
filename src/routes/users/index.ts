961efbb845ffb64fbf1ab1de53d327cb28fb5b4c
Nejc <nejcrogelsek0@gmail.com>
2021-11-07 16:42:00 +0100

    users routes

---
+++ src/routes/users/index.ts
@@ -0,0 +1,67 @@
+import { FastifyPluginAsync } from "fastify"
+import { UpdateOpts, UpdateBody, GetOpts, GetOneOpts, DeleteEventOpts } from './types'
+import * as bcrypt from 'bcrypt'
+import { IUser } from "../../interfaces/user.interface"
+
+const auth: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+	fastify.get('/', GetOpts, async (_, reply) => {
+		const events = await fastify.store.Event.find()
+		if (!events) {
+			return reply.getHttpError(404, 'Cannot find any events.')
+		}
+		return reply.status(200).send({ ...events })
+	})
+
+	fastify.get('/:id', GetOneOpts, async (_, reply) => {
+		const event = await fastify.store.Event.findOne({ _id: 'id from query' })
+		if (!event) {
+			return reply.getHttpError(404, 'Cannot find any events.')
+		}
+		return reply.status(200).send({ ...event.toObject() })
+	})
+
+	fastify.patch<{ Body: UpdateBody }>('/:id', UpdateOpts, async (request, reply) => {
+		const { password } = request.body
+		const user = await fastify.store.User.findOne({ _id: 'query id' })
+		if (!user) {
+			return reply.getHttpError(404, 'Cannot find any users.')
+		}
+
+		if (password) {
+			const hash = await bcrypt.hash(password, 10)
+			user.password = hash
+		}
+
+		user.first_name = request.body.first_name
+		user.last_name = request.body.last_name
+		user.updated_at = new Date()
+
+		user.save((err, user) => {
+			if (err || !user) {
+				return reply.getHttpError('404', 'Cannot Update new user.')
+			}
+			return reply.status(201).send({ ...user.toObject() })
+		})
+
+		return reply
+	})
+
+	fastify.delete('/:id', DeleteEventOpts, async (request, reply) => {
+		const user = await fastify.store.User.findOne({ _id: 'id from querystring' })
+		if (!user) {
+			return reply.getHttpError(404, 'Cannot find any users.')
+		}
+		if (user._id !== 'request user id') {
+			return reply.getHttpError(401, 'Unauthorized access')
+		}
+		user.remove((err: Error, user: IUser) => {
+			if (err || !user) {
+				return reply.getHttpError(404, 'Cannot delete user.')
+			}
+			return reply.status(200).send({ ...user })
+		})
+		return reply
+	})
+}
+
+export default auth;
---
+++ src/routes/users/types.ts
@@ -0,0 +1,63 @@
+import { Type, Static } from '@sinclair/typebox'
+import { RouteShorthandOptions } from 'fastify'
+
+const UpdateRequest = Type.Object({
+	email: Type.String({ format: 'email' }),
+	first_name: Type.String(),
+	last_name: Type.String(),
+	password: Type.Optional(Type.String())
+})
+
+const UpdateResponse = Type.Object({
+	_id: Type.String(),
+	email: Type.String({ format: 'email' }),
+	profile_image: Type.String(),
+	first_name: Type.String(),
+	last_name: Type.String(),
+	email_token: Type.String(),
+	confirmed: Type.Boolean(),
+	password: Type.String(),
+	created_at: Type.String({format:'date-time'}),
+	updated_at: Type.String({format:'date-time'}),
+})
+
+export const UpdateOpts: RouteShorthandOptions = {
+	schema: {
+		body: UpdateRequest,
+		response: {
+			201: UpdateResponse
+		}
+	}
+}
+
+export type UpdateBody = Static<typeof UpdateRequest>
+
+export const DeleteEventOpts: RouteShorthandOptions = {
+	schema: {
+		querystring: {
+			id: { type: 'string' }
+		},
+		response: {
+			200: UpdateResponse
+		}
+	}
+}
+
+export const GetOpts: RouteShorthandOptions = {
+	schema: {
+		response: {
+			200: [UpdateResponse]
+		}
+	}
+}
+
+export const GetOneOpts: RouteShorthandOptions = {
+	schema: {
+		querystring: {
+			id: { type: 'string' }
+		},
+		response: {
+			200: UpdateResponse
+		}
+	}
+}
