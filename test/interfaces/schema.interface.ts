316d5637b5eb149380af84efeef100a2c0a653c2
Nejc <nejcrogelsek0@gmail.com>
2021-12-02 19:47:13 +0100

    clean folder test

---
+++ test/interfaces/schema.interface.ts
@@ -0,0 +1,36 @@
+import * as mongoose from "mongoose";
+
+export interface IUserSchema {
+	_id: string
+	profile_image: string
+	first_name: string
+	last_name: string
+	email: string
+	email_token: string | null
+	confirmed: boolean
+	password: string
+	events: mongoose.Schema.Types.ObjectId
+	created_at: Date
+	updated_at: Date
+}
+export interface IEventSchema {
+	_id: string
+	title: string
+	description: string
+	event_image: string
+	location: string
+	max_visitors: number
+	user_id: mongoose.Schema.Types.ObjectId
+	date_start: string
+	time_start: string
+	url: string
+	created_at: Date
+	updated_at: Date
+}
+
+export interface IReservationSchema {
+	_id: string
+	user_id: number
+	event_id: number
+	created_at: Date
+}
--- test/routes/public.test.ts
+++ test/routes/public.test.ts
@@ -1,7 +1,59 @@
 import { build } from '../helper'
+import { Event } from '../../src/entities/event.entity'
+import * as mongoose from 'mongoose'
+import { User } from '../../src/entities/user.entity'
+import { randomBytes } from 'crypto'
+import { hashSync } from 'bcrypt'
 
 describe('PublicTests', () => {
 	let app = build()
+
+	const UserModel = mongoose.model('User', User)
+	const user = new UserModel({
+		email: 'john@gmail.com',
+		first_name: 'John',
+		last_name: 'Doe',
+		profile_image: 'undefined',
+		password: hashSync('New123!', 10),
+		confirmed: false,
+		email_token: randomBytes(64).toString('hex'),
+		created_at: new Date(),
+		updated_at: new Date()
+	})
+	user.save()
+
+	const EventModel = mongoose.model('Event', Event)
+	const upcoming_event = new EventModel({
+		title: 'Upcoming Event',
+		description: 'This is upcoming event.',
+		event_image: 'upcoming_event.png',
+		location: 'Ljubljana',
+		max_visitors: 10,
+		date_start: '5.12.2021',
+		time_start: '20.00',
+		user_id: user._id,
+		created_at: new Date(),
+		updated_at: new Date()
+	})
+
+	upcoming_event.save()
+	const recent_event = new EventModel({
+		title: 'Recent Event',
+		description: 'This is recent event.',
+		event_image: 'recent_event.png',
+		location: 'Ljubljana',
+		max_visitors: 10,
+		date_start: '5.11.2021',
+		time_start: '20.00',
+		user_id: user._id,
+		created_at: new Date(),
+		updated_at: new Date()
+	})
+	recent_event.save()
+
+	console.log(user)
+	console.log(upcoming_event)
+	console.log(recent_event)
 
 	test('/public/events (GET)', async () => {
 		const res = await app.inject({
@@ -19,6 +71,21 @@ describe('PublicTests', () => {
 		})
 		expect(res.statusCode === 200)
 		expect(200)
+		expect(JSON.parse(res.payload)).toEqual([{
+			_id: expect.any(String),
+			title: expect.any(String),
+			description: expect.any(String),
+			event_image: expect.any(String),
+			location: expect.any(String),
+			max_visitors: expect.any(Number),
+			date_start: expect.any(String),
+			time_start: expect.any(String),
+			user_id: expect.any(String),
+			url: expect.any(String),
+			created_at: expect.any(String),
+			updated_at: expect.any(String),
+			__v: 0
+		}])
 	})
 
 	test('/public/events/recent (GET)', async () => {
@@ -28,11 +95,26 @@ describe('PublicTests', () => {
 		})
 		expect(res.statusCode === 200)
 		expect(200)
+		expect(JSON.parse(res.payload)).toEqual([{
+			_id: expect.any(String),
+			title: expect.any(String),
+			description: expect.any(String),
+			event_image: expect.any(String),
+			location: expect.any(String),
+			max_visitors: expect.any(Number),
+			date_start: expect.any(String),
+			time_start: expect.any(String),
+			user_id: expect.any(String),
+			url: expect.any(String),
+			created_at: expect.any(String),
+			updated_at: expect.any(String),
+			__v: 0
+		}])
 	})
 
 	test('/public/events/:id (GET)', async () => {
 		const res = await app.inject({
-			url: '/public/events/61a3c8948fbd8a5089effa3c',
+			url: `/public/events/${JSON.stringify(upcoming_event._id).split('"')[1]}`,
 			method: 'GET'
 		})
 		expect(res.statusCode === 200)
@@ -64,7 +146,7 @@ describe('PublicTests', () => {
 
 	test('/public/events/url/:url (GET)', async () => {
 		const res = await app.inject({
-			url: '/public/events/url/New-event?q=edacb732-efd6-4f47-874e-e48ffb577e1b',
+			url: `/public/events/${JSON.stringify(upcoming_event.url)}`,
 			method: 'GET'
 		})
 		expect(res.statusCode === 200)
@@ -90,7 +172,7 @@ describe('PublicTests', () => {
 			url: '/public/events/search',
 			method: 'POST',
 			payload: {
-				search_term: 'Sky'
+				search_term: 'upcoming'
 			}
 		})
 		expect(res.statusCode === 201)
